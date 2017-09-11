a530ad0bb6c4a74e3713ce94c5f64b11d9bf6443
Matt Latimer <matt.latimer@gmail.com>
2017-09-11 13:33:33 -0700

    added printReverse function

---
+++ callbacks/print-reverse.js
@@ -0,0 +1,12 @@
+const getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse (html) {
+  console.log(String(html).split('').reverse().join(''));
+}
+
+getHTML(requestOptions, printReverse);

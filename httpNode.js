97ab4308717d6619d6dbafc99e99d6a9aa4c5862
Matt Latimer <matt.latimer@gmail.com>
2017-09-11 12:13:11 -0700

    Now prints in chunks

--- httpNode.js
+++ httpNode.js
@@ -1,8 +1,17 @@
+const https = require('https');
+
 const getAndPrintHTMLChunks = function() {
 
   let requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
-  
+  https.get(requestOptions, (res) => {
+    res.setEncoding('utf-8');
+    res.on('data', (data) => {
+      console.log(data, '\n');
+    });
+  });
 };
+
+getAndPrintHTMLChunks();
